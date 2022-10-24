2d00189e7c1a4e7059e3b2217cba21e9bdd1006a
samuelivanaskas <106975862+samuelivanaskas@users.noreply.github.com>
2022-10-24 14:41:46 -0300

    Create tarefa.js

---
+++ tarefa.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+    $('#carrosel-imagens').slick({
+        autoplay:AnimationEffect
+    });
+
+    $('.menu-hamburguer').click(function() {
+        $('nav').slideToggle();
+    })
+
+    $('#telefone').mask('(00) 00000-0000')
+
+    $('#cpf').mask('000-000-000-00', {
+        placeholder: '___-___-___-__'
+    })
+
+    $('#cep').mask('00000-000', {
+        placeholder: '_____-___'
+    })
+
+    $('form').validate ({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira seu nome completo'
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+
+    $
+})
